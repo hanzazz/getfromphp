5e96fdb86d3625a0697eba75d7d1702d570e5c95
hanzazz <we__j3ack@hotmail.co.th>
2020-09-08 12:39:17 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "sql12.freemysqlhosting.net";
+$username = "sql12363661";
+$password = "2bkjcJHPBS";
+$dbname = "sql12363661";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+	} 
+ $sql = "select UserID,Account,Text,Timestamp from log";
+ $result = $conn->query($sql);
+ 
+ if($result->num_rows>0)//output data of each row
+ {
+	 while($row=$result->fetch_assoc())
+	 {
+		 echo "UserID:" .$row["UserID"]."Account:" .$row["Account"]. "Text:" .$row["Text"]. "Timestamp:" .$row["Timestamp"]. "_" ;
+	 }
+ }
+ else
+ {
+	 echo "0 results";
+ }
+$conn->close();
+
+?>
